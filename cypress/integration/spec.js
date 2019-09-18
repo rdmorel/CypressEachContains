863d1a54ca5705bfe465f706b3e10b3c52ac1a80
Race Morel <rmorel@ccg.ccglink.com>
2019-09-18 13:30:47 -0700

    added failing test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,9 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe('Each+Contains Bug', () => {
+  it('test', () => {
+      cy.visit('https://www.google.com/');
+      cy.get('input[type="submit"]').each(($input) => {
+          cy.wrap($input).should('exist')
+      });
+      cy.contains('About');
+  });
+});
