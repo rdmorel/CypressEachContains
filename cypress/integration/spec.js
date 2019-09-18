9f03cf06dcea283537c13357b3c1379f7ce4085f
Race Morel <rmorel@ccg.ccglink.com>
2019-09-18 13:57:50 -0700

    edited for clarity

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,9 +1,10 @@
-describe('Each+Contains Bug', () => {
+describe('Each + Contains Bug', () => {
   it('test', () => {
       cy.visit('https://www.google.com/');
+      cy.contains('About').should('be.visible');
       cy.get('input[type="submit"]').each(($input) => {
           cy.wrap($input).should('exist')
       });
-      cy.contains('About');
+      cy.contains('About').click();
   });
 });
